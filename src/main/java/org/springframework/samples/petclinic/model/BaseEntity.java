455d9b9311c0105f22b1f57a92c1db865c56125d
Shashank Verma <shashankverma1816@gmail.com>
2024-11-27 12:04:57 +0530

    Updating code via API

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -6,26 +6,71 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * BaseEntity serves as a base class for all entities in the application.
+ * It provides common properties and methods that can be shared across different entities.
+ */
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; // Serialization ID for the class
 
+    // Logger instance for logging messages related to this class
+    private static final Logger logger = LoggerFactory.getLogger(BaseEntity.class);
+
+    // Unique identifier for the entity, automatically generated
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    /**
+     * Gets the ID of the entity.
+     * 
+     * @return the ID of the entity
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the entity.
+     * If the provided ID is null, a warning is logged.
+     * 
+     * @param id the ID to set
+     */
     public void setId(Integer id) {
-        this.id = id;
+        if (id == null) {
+            // Log a warning if an attempt is made to set a null ID
+            logger.warn("Attempted to set a null ID on entity: {}", this);
+        }
+        this.id = id; // Set the ID regardless of null check
     }
 
+    /**
+     * Checks if the entity is new (i.e., has not been persisted yet).
+     * Logs the status of the entity.
+     * 
+     * @return true if the entity is new, false otherwise
+     */
     public boolean isNew() {
-        return this.id == null;
+        boolean isNew = this.id == null; // Determine if the entity is new
+        // Log the new status of the entity
+        logger.debug("Entity {} is new: {}", this, isNew);
+        return isNew; // Return the new status
+    }
+
+    /**
+     * Provides a string representation of the BaseEntity.
+     * This is useful for logging and debugging purposes.
+     * 
+     * @return a string representation of the entity
+     */
+    @Override
+    public String toString() {
+        return "BaseEntity{id=" + id + "}"; // Return the ID in the string representation
     }
 }
 
