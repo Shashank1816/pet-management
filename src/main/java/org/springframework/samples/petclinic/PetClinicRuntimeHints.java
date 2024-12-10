feb33a11805f163ddf042a5abd8289774ce3eb19
Shaik-Kareem-89 <kareemshaik0809@gmail.com>
2024-12-10 16:57:21 +0530

    Removed pushed code from PetClinicRuntimeHints.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.samples.petclinic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.samples.petclinic.model.BaseEntity;
@@ -10,52 +24,15 @@ import org.springframework.samples.petclinic.vet.Vet;
 
 public class PetClinicRuntimeHints implements RuntimeHintsRegistrar {
 
-    private static final Logger logger = LoggerFactory.getLogger(PetClinicRuntimeHints.class);
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		hints.resources().registerPattern("db/*"); // https://github.com/spring-projects/spring-boot/issues/32654
+		hints.resources().registerPattern("messages/*");
+		hints.resources().registerPattern("META-INF/resources/webjars/*");
+		hints.resources().registerPattern("mysql-default-conf");
+		hints.serialization().registerType(BaseEntity.class);
+		hints.serialization().registerType(Person.class);
+		hints.serialization().registerType(Vet.class);
+	}
 
-    @Override
-    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        try {
-            registerResourcePatterns(hints.resources());
-            registerSerializationTypes(hints.serialization());
-        } catch (Exception e) {
-            logger.error("Error registering runtime hints", e);
-        }
-    }
-
-    private void registerResourcePatterns(RuntimeHints.Resources resources) {
-        try {
-            registerDatabaseResources(resources);
-            registerMessageResources(resources);
-            registerWebJarsResources(resources);
-            registerDefaultConfResource(resources);
-        } catch (Exception e) {
-            logger.error("Error registering resource patterns", e);
-        }
-    }
-
-    private void registerDatabaseResources(RuntimeHints.Resources resources) {
-        resources.registerPattern("db/*");
-    }
-
-    private void registerMessageResources(RuntimeHints.Resources resources) {
-        resources.registerPattern("messages/*");
-    }
-
-    private void registerWebJarsResources(RuntimeHints.Resources resources) {
-        resources.registerPattern("META-INF/resources/webjars/*");
-    }
-
-    private void registerDefaultConfResource(RuntimeHints.Resources resources) {
-        resources.registerPattern("mysql-default-conf");
-    }
-
-    private void registerSerializationTypes(RuntimeHints.Serialization serialization) {
-        try {
-            serialization.registerType(BaseEntity.class);
-            serialization.registerType(Person.class);
-            serialization.registerType(Vet.class);
-        } catch (Exception e) {
-            logger.error("Error registering serialization types", e);
-        }
-    }
 }
