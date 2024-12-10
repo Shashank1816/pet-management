e3d90b9407ecf7c27bb004a845e709b67d2cf213
Shaik-Kareem-89 <kareemshaik0809@gmail.com>
2024-12-10 16:42:17 +0530

    removed pushed code from BaseEntity.java

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -1,44 +1,51 @@
-
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.samples.petclinic.model;
 
 import java.io.Serializable;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+/**
+ * Simple JavaBean domain object with an id property. Used as a base class for objects
+ * needing this property.
+ *
+ * @author Ken Krebs
+ * @author Juergen Hoeller
+ */
 @MappedSuperclass
-public abstract class BaseEntity implements Serializable {
+public class BaseEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private static final Logger logger = LoggerFactory.getLogger(BaseEntity.class);
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	public Integer getId() {
+		return id;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setId(Integer id) {
-        if (id == null) {
-            logger.warn("Attempted to set a null ID on entity: {}", this);
-        }
-        this.id = id;
-    }
+	public boolean isNew() {
+		return this.id == null;
+	}
 
-    public boolean isNew() {
-        boolean isNew = (this.id == null);
-        logger.debug("Entity {} is new: {}", this, isNew);
-        return isNew;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("BaseEntity{id=%d}", id);
-    }
 }
-
