7da4089a155046d0dc3aac0ea1ad5ab15c7b1415
Shaik-Kareem-89 <kareemshaik0809@gmail.com>
2024-12-10 16:56:22 +0530

    removed pushed code from PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,32 +1,37 @@
-To enhance modularity and separation of concerns in the Spring application, we can refactor the code by creating a dedicated configuration class for the application. This will help in organizing the application setup and make it easier to manage different configurations in the future. 
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Here’s the refactored code:
-
-```javascript
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.context.annotation.Configuration;
 
+/**
+ * PetClinic Spring Boot Application.
+ *
+ * @author Dave Syer
+ *
+ */
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
 }
-
-@Configuration
-class PetClinicConfig {
-    // Additional configuration can be added here in the future
-}
-```
-
-### Changes Made:
-1. **Modularization**: Introduced a separate configuration class `PetClinicConfig` to allow for future configurations and to adhere to the Single Responsibility Principle.
-2. **Code Organization**: This separation makes it easier to manage configurations and enhances readability.
-
-This refactoring sets the stage for further modularization as the application grows, allowing for better organization of configuration-related code.
